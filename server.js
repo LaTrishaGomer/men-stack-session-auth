af98b211d9f6f5ce18d98bed862b642f4638aa40
latrisha <l_gomer@yahoo.com>
2025-03-10 11:00:09 -0700

    wip: adds root route

---
+++ server.js
@@ -0,0 +1,34 @@
+//dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//initialize express app
+const app = express();
+
+//configure settings
+dotenv.config();
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+//connect to mongoDB
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB ${mongoose.connection.name}');
+});
+
+//mount middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+
+//mount routes
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+//tell the app to listen for HTTP requests
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+  });
